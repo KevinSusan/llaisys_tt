5bf8cf9ef0d0c2bc1eca9531875f11b77fcd6921
lain <2030746443@qq.com>
2026-03-15 00:09:31 +0800

    fix: use custom rule for iluvatar clang++ compilation

--- xmake/iluvatar.lua
+++ xmake/iluvatar.lua
@@ -3,17 +3,15 @@ target("llaisys-device-iluvatar")
     add_deps("llaisys-utils")
     set_languages("cxx17")
     set_warnings("all", "error")
-    add_cxflags("-fPIC", "-Wno-unknown-pragmas")
 
-    -- Iluvatar CoreX uses clang++ with CUDA frontend
-    set_toolchains("clang")
-    add_cxflags("-x", "cuda", "--cuda-gpu-arch=ivcore10", "--cuda-path=/usr/local/corex", {force = true})
     add_includedirs("/usr/local/corex/include")
     add_linkdirs("/usr/local/corex/lib64")
     add_links("cudart")
 
-    add_files("../src/device/iluvatar/iluvatar_runtime_api.cu")
-    add_files("../src/device/iluvatar/iluvatar_resource.cu")
+    add_files("../src/device/iluvatar/*.cu", {
+        rule = "iluvatar_cu"
+    })
+
     on_install(function (target) end)
 target_end()
 
@@ -22,15 +20,44 @@ target("llaisys-ops-iluvatar")
     add_deps("llaisys-tensor")
     set_languages("cxx17")
     set_warnings("all", "error")
-    add_cxflags("-fPIC", "-Wno-unknown-pragmas")
 
-    -- Iluvatar CoreX uses clang++ with CUDA frontend
-    set_toolchains("clang")
-    add_cxflags("-x", "cuda", "--cuda-gpu-arch=ivcore10", "--cuda-path=/usr/local/corex", {force = true})
     add_includedirs("/usr/local/corex/include")
     add_linkdirs("/usr/local/corex/lib64")
     add_links("cudart")
 
-    add_files("../src/ops/*/nvidia/*.cu")
+    add_files("../src/ops/*/nvidia/*.cu", {
+        rule = "iluvatar_cu"
+    })
+
     on_install(function (target) end)
 target_end()
+
+rule("iluvatar_cu")
+    set_extensions(".cu")
+    on_build_file(function (target, sourcefile, opt)
+        import("core.project.depend")
+        import("core.tool.compiler")
+
+        local objectfile = target:objectfile(sourcefile)
+        local dependfile = target:dependfile(objectfile)
+
+        depend.on_changed(function ()
+            local argv = {
+                "-x", "cuda",
+                "--cuda-gpu-arch=ivcore10",
+                "--cuda-path=/usr/local/corex",
+                "-std=c++17",
+                "-fPIC",
+                "-O3",
+                "-DENABLE_ILUVATAR_API",
+                "-Iinclude",
+                "-I/usr/local/corex/include",
+                "-c",
+                "-o", objectfile,
+                sourcefile
+            }
+
+            os.vrunv("/usr/local/corex/bin/clang++", argv)
+        end, {dependfile = dependfile, files = {sourcefile}})
+    end)
+rule_end()
